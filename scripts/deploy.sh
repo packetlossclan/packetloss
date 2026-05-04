8cd7efb3441b7b9c93d65724f4ac30ce31239393
Lucas Saliés Brum <sistematico@gmail.com>
2026-05-03 20:14:45 -0400

    chore: update chcon command path for consistency in deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -40,7 +40,7 @@ if pnpm run build; then
   # Ensure Nginx can read static assets served directly (images, audio)
   #sudo /usr/sbin/restorecon -Rv /home/nginx/.local/share/pnpm/
   #sudo /usr/sbin/restorecon -v /home/nginx/.local/share/pnpm/pnpm
-  sudo /usr/sbin/chcon -t bin_t /home/nginx/.local/share/pnpm/pnpm
+  sudo /usr/bin/chcon -t bin_t /home/nginx/.local/share/pnpm/pnpm
 
   sudo /usr/bin/systemctl start $SERVICE
   echo "🚀 Serviço reiniciado!"
